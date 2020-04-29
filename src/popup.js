99ff1203bf02510a26f09313e3f7bfb5eb691a59
Hshen <hackshen.com@gmail.com>
2020-04-29 14:49:44 +0800

    qrcode react

--- src/popup.js
+++ src/popup.js
@@ -1,29 +1,67 @@
-const getQr = (url) => {
-    return `http://api.hackshen.com/qrcode?data=${url}`
+import React, {useState, useEffect} from 'react'
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import "babel-polyfill";
+import './popup.css';
+
+export default function App(props) {
+    const [imgUrl, setImgUrl] = useState('');
+    const [message, setMessage] = useState('');
+    const [url, setUrl] = useState('');
+
+    const getMsg = () => {
+        return axios('http://api.hackshen.com:3000/message');
+    }
+
+    const getQr = (url) => {
+        return `http://api.hackshen.com:3000/qrcode?data=${url}`
+    }
+
+    const messag = async () => {
+        const msg = await getMsg();
+        setMessage(msg.data[0].title);
+    }
+
+    const getValue = (e) => {
+        const value = e.target.value;
+        setUrl(value);
+        setImgUrl(getQr(value));
+    }
+    useEffect(() => {
+        chrome.tabs.getSelected(null, (tab) => {
+            window.tab = tab
+            window.tabUrl = tab.url;
+            setImgUrl(getQr(tab.url));
+            setUrl(tab.url);
+        });
+        messag();
+        // const msg = getMsg();
+        // msg.then((res)=>{
+        //     setMessage(res.data[0].title);
+        // })
+    }, []);
+
+    return (
+        <div>
+            <img id="qrcode" src={imgUrl} alt="" style={{width: '240px'}}/>
+            <div id="qrcode1"></div>
+            <div className="qrtext">Current qr code</div>
+
+            <div className="changeInput">
+                <textarea id="urlVal" style={{maxWidth: '90%'}} type="text" value={url} onChange={(e) => {
+                    getValue(e)
+                }}/>
+            </div>
+            <div className="message" onClick={() => {
+                messag()
+            }}>{message}</div>
+            <div style={{textAlign: "center"}}>
+                <a href="http://hackshen.com" target="_blank">Author: Hshen</a>
+            </div>
+        </div>
+    )
 }
 
-const getMsg = ()=>{
-    $.get('http://api.hackshen.com/message').then((res) => {
-        console.log(res)
-        $('.message').html(res[0].title)
-    })
-}
-
-const setQr = () => {
-
-}
-chrome.tabs.getSelected(null, (tab) => {
-    window.tab = tab
-    $('#qrcode').attr('src', getQr(tab.url))
-});
-
-getMsg();
-
-$('#urlCode').on('click', () => {
-    const url = getQr($('#urlVal').val());
-    $('#qrcode').attr('src', url)
-})
-
-$('.message').on('click', () => {
-    getMsg();
-});
+ReactDOM.render(
+    <App/>
+    , document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,89 @@
+const path = require('path');
+const env = process.env.NODE_ENV;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const copyWpackPlugin = require('copy-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const cleanPlugin =  new CleanWebpackPlugin();
+const copyPlugin = new copyWpackPlugin(
+    //接受一个数组 可以多个文件
+    [{
+        from: './src/dist',
+        to: './'
+    }]
+)
+const htmlPlugin = new HtmlWebpackPlugin({
+    filename: 'popup.html',
+    template: './src/popup.html',
+    // minify: {
+    // removeAttributeQuotes:true,
+    // collapseWhitespace:true,
+    // },
+    // hash : true
+});
+
+const cssPlugin = new MiniCssExtractPlugin({
+    filename: env === 'dev' ? 'index.css' : 'index.[hash:8].css'
+});
+
+module.exports = {
+    mode:  env === 'dev' ? 'development' : 'production',
+    entry: {
+        popup: './src/popup.js'
+    },
+
+    output: {
+        path: path.join(__dirname, './build'),
+        filename: env === 'dev' ? '[name].js' : '[name].[hash:8].js'
+    },
+    // watch: true,
+    // watchOptions: {
+    //     poll: 1000,  // 每秒查询1000次
+    //     aggregateTimeout: 500, // 防抖，节流
+    //     ignored: /node_modules/  //不需要监控的文件
+    // },
+    devtool: env === 'dev' ? 'cheap-module-source-map' : 'source-map',
+    devServer: {
+        host: 'localhost',
+        port: '7766',
+        progress: true,
+        open: false,
+        hot: true,
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css', '.scss', '.json'],
+        alias: {
+            //配置后可以直接用@来表示这个位置,直接应用目录下得文件,是绝对路径
+            '@': path.join(__dirname, './src')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.md$/,
+                use: "raw-loader"
+            },
+            {
+                test: /\.(js|jsx)$/,
+                use: {loader: 'babel-loader'},
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            }, {
+                test: /\.(png|jpg|gif)/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        name: '[hash:8].[ext]',
+                        outputPath: 'img',
+                        limit: 500000
+                    }
+                }]
+            }
+        ]
+    },
+    plugins: [htmlPlugin, cssPlugin, copyPlugin, env === 'dev' ? '' : cleanPlugin]
+}
