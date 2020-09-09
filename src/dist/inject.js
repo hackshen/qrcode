117381618499553878460a89eb293cefc4ca757e
Hshen <hackshen.com@gmail.com>
2020-09-09 14:43:49 +0800

    Update inject.js

--- src/dist/inject.js
+++ src/dist/inject.js
@@ -1,1 +1,24 @@
-console.log('Test');
+const options = {
+    'jQueryURL': 'https://libs.baidu.com/jquery/2.0.0/jquery.min.js'
+};
+console.log('inject!');
+chrome.extension.onMessage.addListener((request, sender, sendResponse) => {
+        // if (request.action == "GetBaiduKeyWord") {
+        //     sendResponse({kw: document.forms[0].wd.value});
+        // }
+        if (request.action == 'inject') {
+            const script = document.createElement('script');
+            script.src = options["jQueryURL"];
+            script.type = 'text/javascript';
+
+            script.onload = function () {
+                console.log("jQuery(" + options["jQueryURL"] + ") loaded.");
+            };
+            script.onerror = function () {
+                console.log("Error while loading jQuery(" + options["jQueryURL"] + ").");
+            };
+            document.head.appendChild(script);
+            sendResponse({msg: 'success!'});
+        }
+    }
+);
