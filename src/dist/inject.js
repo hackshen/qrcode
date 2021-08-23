d9e7bdc5ed579ecd1fe4dd595365ef8f87206363
Hshen <hackshen.com@gmail.com>
2021-08-23 21:32:40 +0800

    Update

--- src/dist/inject.js
+++ src/dist/inject.js
@@ -1,26 +1,33 @@
+
 const options = {
     'jQueryURL': 'https://libs.baidu.com/jquery/2.0.0/jquery.min.js'
 };
+const loadScript = (src, callback) => {
+    let script = document.createElement('script');
+    script.src = src;
+    script.type = 'text/javascript';
+    script.onload = () => callback && callback(null, script);
+    script.onerror = () => callback && callback(new Error(`Script load error for ${src}`));
+    document.head.append(script);
+};
+
 console.log('inject!');
 chrome.extension.onMessage.addListener((request, sender, sendResponse) => {
+        const {action} = request;
         // if (request.action == "GetBaiduKeyWord") {
         //     sendResponse({kw: document.forms[0].wd.value});
         // }
-        if (request.action == 'inject') {
-            const script = document.createElement('script');
-            script.src = options["jQueryURL"];
-            script.type = 'text/javascript';
-
-            script.onload = function () {
-                console.log("jQuery(" + options["jQueryURL"] + ") loaded.");
-            };
-            script.onerror = function () {
-                console.log("Error while loading jQuery(" + options["jQueryURL"] + ").");
-            };
-            document.head.appendChild(script);
-            sendResponse({msg: 'success!'});
+        if (action === 'inject') {
+            loadScript(options.jQueryURL, (error, script) => {
+                if (error) {
+                    console.log(error);
+                } else {
+                    sendResponse({msg: 'success!'});
+                    console.log("jQuery(" + options["jQueryURL"] + ") loaded.");
+                }
+            });
         }
-        if (request.action === 'clear') {
+        if (action === 'clear') {
             alert('DNS缓存清除成功!');
         }
     }
